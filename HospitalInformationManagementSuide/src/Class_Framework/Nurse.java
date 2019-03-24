7cc8dcff9939a2a49e815406816750a179b1d7f6
anton96121 <anton96121@gmail.com>
2019-03-24 14:23:20 +0100

    Til Mads

--- HospitalInformationManagementSuide/src/Class_Framework/Nurse.java
+++ HospitalInformationManagementSuide/src/Class_Framework/Nurse.java
@@ -4,11 +4,11 @@ public class Nurse extends Staff{
 	public Nurse(String firstName, String lastName,String adress, String tribe, int day, int month, int year) {
 		this.setFirstName(firstName);
 		this.setLastName(lastName);
-		this.setJobType("Nurse");// It's very important that you first define job type, then ID and lastly email because they use each other
-		this.setID("N");
 		this.setBirthDay(day, month, year);
 		this.setTribe(tribe);
 		this.setAdress(adress);
+		this.setJobType("Nurse");// It's very important that you first define job type, then ID and lastly email because they use each other
+		this.setID("N");
 		this.setEmail();
 	}
 
