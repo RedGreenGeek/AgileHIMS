ee5ef4b8a243665fb4f79f0b40e07bff7da4af41
anton96121 <anton96121@gmail.com>
2019-03-24 13:22:55 +0100

    Person framework with comments

--- HospitalInformationManagementSuide/src/Class_Framework/Doctor.java
+++ HospitalInformationManagementSuide/src/Class_Framework/Doctor.java
@@ -4,7 +4,7 @@ public class Doctor extends Staff{
 	public Doctor(String firstName, String lastName) {
 		this.setFirstName(firstName);
 		this.setLastName(lastName);
-		this.setJobType("Doctor");
+		this.setJobType("Doctor");// It's very important that you first define job type, then ID and lastly email because they use each other
 		this.setID("D");
 		this.setEmail();
 	}
