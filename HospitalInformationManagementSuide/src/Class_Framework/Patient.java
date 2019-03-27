91b46e322d00001133fdb5e2b8464e95e77bc36d
s <s@s.s>
2019-03-27 10:27:09 +0100

    Ready for creating production module.

--- HospitalInformationManagementSuide/src/Class_Framework/Patient.java
+++ HospitalInformationManagementSuide/src/Class_Framework/Patient.java
@@ -1,9 +1,24 @@
 package Class_Framework;
 
+
 public class Patient extends Person {
-	private int patientID;
-	private static int counter;
-	
+private int patientID;
+private static int counter;
+
+public int getPatientID() {
+    return this.patientID;
+}
+
+public Patient(String firstName, String lastName, String adress, String tribe, int day, int month, int year, boolean alive) {
+    this.setFirstName(firstName);
+    this.setLastName(lastName);
+    this.setAlive(alive);
+    this.setBirthDay(day, month, year);
+    this.setTribe(tribe);
+    this.setAdress(adress);
+    Patient.counter +=1;
+    this.patientID = Patient.counter;
+    
 }
 
 }
