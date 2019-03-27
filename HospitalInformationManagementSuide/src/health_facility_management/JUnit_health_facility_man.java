eb383261a34fe94e37636d5d7bd8a43dbf4b23ba
s <s@s.s>
2019-03-27 21:31:08 +0100

    Updated JUnit and some functioncalls.
    Worked through the JUnit to accommodate the changes to classes.

---
+++ HospitalInformationManagementSuide/src/health_facility_management/JUnit_health_facility_man.java
@@ -0,0 +1,14 @@
+package health_facility_management;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JUnit_health_facility_man {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
