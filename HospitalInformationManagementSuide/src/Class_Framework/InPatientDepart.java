4b6d0160f727012c6d6150d66cd972f83d88e59b
s <s@s.s>
2019-03-28 03:13:33 +0100

    M3_Implemented
    Made a small inconsequential change to ChangeReg and InPatientDep

--- HospitalInformationManagementSuide/src/Class_Framework/InPatientDepart.java
+++ HospitalInformationManagementSuide/src/Class_Framework/InPatientDepart.java
@@ -3,14 +3,26 @@ package Class_Framework;
 import java.util.HashSet;
 
 public class InPatientDepart extends HCDepart{
-	int maxBeds;
-	int bedsInUse;
+	private int maxBeds;
+	private int bedsInUse;
 	
 	public InPatientDepart(String departName, HashSet<Staff> staffSet, HashSet<Patient> patientSet, int maxBeds) {
 		this.departName = departName;
 		this.staffSet = staffSet;
 		this.patientSet = patientSet;
 		this.maxBeds = maxBeds;
+	}
+	
+	public int getMaxBeds() {
+		return maxBeds;
+	}
+
+	public int getBedsInUse() {
+		return bedsInUse;
+	}
+
+	public void setBedsInUse(int bedsInUse) {
+		this.bedsInUse = bedsInUse;
 	}
 }
 
---
+++ HospitalInformationManagementSuide/src/health_facility_management/BedsInUse.java
@@ -0,0 +1,13 @@
+package health_facility_management;
+
+
+import Class_Framework.InPatientDepart;
+
+
+public class BedsInUse {
+	
+	int getBedsinUse(InPatientDepart IPT) {
+		return IPT.getBedsInUse();
+	}
+
+}
