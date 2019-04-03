43a253b8d5b94ba57840b3ac949b88460cdb1084
Andreas Engly <s170303@student.dtu.dk>
2019-04-03 13:28:43 +0200

    API

---
+++ HospitalInformationManagementSuide/src/Class_Framework/API.java
@@ -0,0 +1,28 @@
+package Class_Framework;
+
+
+public class API {
+	//Clerk, ITC, 
+	public void RegisterPatient(String firstname, String lastname, String address, String tribe) {
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
